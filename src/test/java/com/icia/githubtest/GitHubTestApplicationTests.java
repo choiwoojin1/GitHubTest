cba4d1ac36b89a695f068315642aeb4f3e9b6eb7
choiwoojin <dkelwhgdma@naver.com>
2021-12-14 18:33:22 +0900

    IntelliJ GitHub Test

---
+++ src/test/java/com/icia/githubtest/GitHubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
